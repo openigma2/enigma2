702224f56335229aa90cf3b0e1d9844960c90ef0
WanWizard <wanwizard@openpli.org>
2024-11-17 20:49:51 +0000

    [multiboot] don't display revision info for release images

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -55,8 +55,10 @@ def getSlotImageInfo(slot, imagedir="/"):
 		BuildType = BoxInfoInstance.getItem("imagetype", "")[0:3]
 		BuildVer = BoxInfoInstance.getItem("imagebuild")
 		BuildDate = estimateSlotImageDate(imagedir, BoxInfoInstance.getItem("compiledate"), BuildVer)
-		BuildDev = str(idb).zfill(3) if BuildType and BuildType != "rel" and (idb := BoxInfoInstance.getItem("imagedevbuild")) else ""
-		return " ".join([str(x).strip() for x in (Creator, BuildImgVersion, BuildType, BuildDev, BuildVer, BuildDate) if x and str(x).strip()])
+		BuildDev = str(idb).zfill(3) if BuildType and BuildType != "rev" and (idb := BoxInfoInstance.getItem("imagedevbuild")) else ""
+		if BuildType == "rev":
+			return " ".join([str(x).strip() for x in (Creator, BuildImgVersion, BuildType, BuildVer, BuildDate) if x and str(x).strip()])
+		return " ".join([str(x).strip() for x in (Creator, BuildImgVersion, BuildDate) if x and str(x).strip()])
 	else:
 		print("[multiboot] [GetImagelist] using etc/issue")
 		try:
